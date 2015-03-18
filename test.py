01674b58c57d041e117e86c38cad7e99035eb0d2
funs <pc904b28@yahoo.com.tw>
2015-03-18 18:29:29 +0800

    some change of test1 and 3

--- test.py
+++ test.py
@@ -6,11 +6,38 @@ import mechanize
 import time
 
 br = mechanize.Browser()
+
+br.set_handle_equiv(True)
+br.set_handle_redirect(True)
+br.set_handle_referer(True)
 br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+# Turn on debugging messages
 br.set_debug_http(True)
-br.set_debug_responses(True)
 br.set_debug_redirects(True)
-br.addheaders = [('User-agent', 'Mozilla/5.0')]
+br.set_debug_responses(True)
+
+br.addheaders = [("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36")]
+
+
+br.open('https://login.yahoo.com/')
+#br.open('http://auderio.com/')
+#print(openpage.read())
+br.select_form(nr=0)
+print('start')
+for field_name in br.form.controls:
+    print field_name
+#br.form['username'] = 'lalalala'
+#br.form['passwd'] = 'llla2'
+#br.form.find_control(id='inputs').__setattr__('value', 'https://www.youtube.com/watch?v=g4FMAgeVV_w')
+
+#br.submit()
+aaa = br.response().read()
+bbb = br.response().get_data()
+print(bbb)
+for link in br.links():
+    print(link)
+
 '''
 br.open('https://www.google.com.tw/')
 
@@ -30,20 +57,6 @@ print(aaa)
 for link in br.links():
     print(link)
 '''
-
-br.open('https://savedeo.com/en')
-#br.open('http://auderio.com/')
-br.select_form(nr=0)
-br.form['url'] = 'https://www.youtube.com/watch?v=g4FMAgeVV_w'
-print("")
-#br.form.find_control(id='youtube-url').__setattr__('value', 'https://www.youtube.com/watch?v=g4FMAgeVV_w')
-
-br.submit()
-aaa = br.response().read()
-bbb = br.response().get_data()
-print(bbb)
-for link in br.links():
-    print(link)
 
 '''
 url = "http://auderio.com/download?url=https://www.youtube.com/watch?v=g4FMAgeVV_w"
--- test3.py
+++ test3.py
@@ -1,43 +1,24 @@
 __author__ = 'ITRI'
-# coding=utf8
-
-__author__ = 'ITRI'
 # coding=utf-8
 
+import urllib
 import urllib2
-import mechanize
-import time
+import requests
+from pprint import pprint
 
-br = mechanize.Browser()
+uri = 'https://tw.yahoo.com/'
+# Prepare the data
+values = {'p':'柯文哲'}
+data = urllib.urlencode(values)
+headers = {'User-agent':'Mozilla/11.0'}
 
-br.set_handle_equiv(True)
-br.set_handle_redirect(True)
-br.set_handle_referer(True)
-br.set_handle_robots(False)
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-# Turn on debugging messages
-br.set_debug_http(True)
-br.set_debug_redirects(True)
-br.set_debug_responses(True)
+request = urllib2.Request(uri, data=data, headers=headers)
+response = urllib2.urlopen(request)
 
-br.addheaders = [('User-agent', 'Mozilla/5.0')]
-
-
-br.open('https://tw.yahoo.com/')
-br.select_form(nr=0)
-print('start')
+html = response.read()
 '''
-for field_name in br.form.controls:
-    print field_name
+
+response = requests.post(uri,data=data,headers=headers)
+html = response.content
 '''
-br.form['p'] = 'line'
-#br.form['passwd'] = 'tpe1234'
-#br.form.find_control(id='inputs').__setattr__('value', 'https://www.youtube.com/watch?v=g4FMAgeVV_w')
-
-br.submit()
-aaa = br.response().read()
-bbb = br.response().get_data()
-print(aaa)
-for link in br.links():
-    print(link)
-
+print(html)
